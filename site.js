7a77e77443fd4ffc4a9261793e94512693fa4e47
Peter Kieltyka <peter.kieltyka@pressly.com>
2016-03-09 20:46:14 -0500

    Fix release link

--- site.js
+++ site.js
@@ -7,7 +7,7 @@ github.get("repos/pressly/sup/tags", function (err, tags) {
     childElem = document.createElement('strong'),
     latestTag = tags[0];
 
-    elem.setAttribute('href', elem.getAttribute('href') + '/tags/' + latestTag.name);
+    elem.setAttribute('href', elem.getAttribute('href') + '/tag/' + latestTag.name);
     elem.innerHTML = 'Latest release'
     childElem.innerHTML = latestTag.name.replace('v','');
     elem.appendChild(childElem);
