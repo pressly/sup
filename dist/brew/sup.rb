6f543d9f7c815a193d5d881507ad2d5b6461b1ae
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2016-04-29 14:43:33 -0400

    Remove deps from brew, we use vendor/ directory now

--- dist/brew/sup.rb
+++ dist/brew/sup.rb
@@ -1,27 +1,13 @@
 require "language/go"
 
 class Sup < Formula
-  desc "Super simple deployment tool - just Unix - think of it like 'make' for a network of servers."
+  desc "Stack Up. Super simple deployment tool - think of it like 'make' for a network of servers."
   homepage "https://github.com/pressly/sup"
   url "https://github.com/pressly/sup/archive/4ee5083c8321340bc2a6410f24d8a760f7ad3847.zip"
   version "0.3.1"
   sha256 "7fa17c20fdcd9e24d8c2fe98081e1300e936da02b3f2cf9c5a11fd699cbc487e"
 
   depends_on "go"  => :build
-
-  go_resource "github.com/goware/prefixer" do
-    url "https://github.com/goware/prefixer.git",
-      :revision => "395022866408d928fc2439f7eac73dd8d370ec1d"
-  end
-
-  go_resource "golang.org/x/crypto" do
-    url "https://go.googlesource.com/crypto.git",
-      :revision => "3760e016850398b85094c4c99e955b8c3dea5711"
-  end
-
-  go_resource "gopkg.in/yaml.v2" do
-    url "https://gopkg.in/yaml.v2.git", :revision => "f7716cbe52baa25d2e9b0d0da546fcf909fc16b4"
-  end
 
   def install
     ENV["GOBIN"] = bin
