53c5d28ecf66e78b09cc27aca89cc1b29aa69e20
Štěpán Pilař <ja@mingan.name>
2018-03-23 14:52:16 +0100

    Reorder functions for readability

--- cmd/sup/main.go
+++ cmd/sup/main.go
@@ -71,141 +71,6 @@ func init() {
 
 }
 
-func networkUsage(conf *sup.Supfile) {
-	w := &tabwriter.Writer{}
-	w.Init(os.Stderr, 4, 4, 2, ' ', 0)
-	defer w.Flush()
-
-	// Print available networks/hosts.
-	fmt.Fprintln(w, "Networks:\t")
-	for _, name := range conf.Networks.Names {
-		fmt.Fprintf(w, "- %v\n", name)
-		network, _ := conf.Networks.Get(name)
-		for _, host := range network.Hosts {
-			fmt.Fprintf(w, "\t- %v\n", host)
-		}
-	}
-	fmt.Fprintln(w)
-}
-
-func cmdUsage(conf *sup.Supfile) {
-	w := &tabwriter.Writer{}
-	w.Init(os.Stderr, 4, 4, 2, ' ', 0)
-	defer w.Flush()
-
-	// Print available targets/commands.
-	fmt.Fprintln(w, "Targets:\t")
-	for _, name := range conf.Targets.Names {
-		cmds, _ := conf.Targets.Get(name)
-		fmt.Fprintf(w, "- %v\t%v\n", name, strings.Join(cmds, " "))
-	}
-	fmt.Fprintln(w, "\t")
-	fmt.Fprintln(w, "Commands:\t")
-	for _, name := range conf.Commands.Names {
-		cmd, _ := conf.Commands.Get(name)
-		fmt.Fprintf(w, "- %v\t%v\n", name, cmd.Desc)
-	}
-	fmt.Fprintln(w)
-}
-
-// parseArgs parses args and returns network and commands to be run.
-// On error, it prints usage and exits.
-func parseArgs(args []string, conf *sup.Supfile) (*sup.Network, []*sup.Command, error) {
-	var commands []*sup.Command
-
-	if len(args) < 1 {
-		networkUsage(conf)
-		return nil, nil, ErrUsage
-	}
-
-	// Does the <network> exist?
-	network, ok := conf.Networks.Get(args[0])
-	if !ok {
-		networkUsage(conf)
-		return nil, nil, ErrUnknownNetwork
-	}
-
-	hosts, err := network.ParseInventory()
-	if err != nil {
-		return nil, nil, err
-	}
-	network.Hosts = append(network.Hosts, hosts...)
-
-	// Does the <network> have at least one host?
-	if len(network.Hosts) == 0 {
-		networkUsage(conf)
-		return nil, nil, ErrNetworkNoHosts
-	}
-
-	// Check for the second argument
-	if len(args) < 2 {
-		cmdUsage(conf)
-		return nil, nil, ErrUsage
-	}
-
-	// In case of the network.Env needs an initialization
-	if network.Env == nil {
-		network.Env = make(sup.EnvList, 0)
-	}
-
-	// Add default env variable with current network
-	network.Env.Set("SUP_NETWORK", args[0])
-
-	// Add default nonce
-	network.Env.Set("SUP_TIME", time.Now().UTC().Format(time.RFC3339))
-	if os.Getenv("SUP_TIME") != "" {
-		network.Env.Set("SUP_TIME", os.Getenv("SUP_TIME"))
-	}
-
-	// Add user
-	if os.Getenv("SUP_USER") != "" {
-		network.Env.Set("SUP_USER", os.Getenv("SUP_USER"))
-	} else {
-		network.Env.Set("SUP_USER", os.Getenv("USER"))
-	}
-
-	for _, cmd := range args[1:] {
-		// Target?
-		target, isTarget := conf.Targets.Get(cmd)
-		if isTarget {
-			// Loop over target's commands.
-			for _, cmd := range target {
-				command, isCommand := conf.Commands.Get(cmd)
-				if !isCommand {
-					cmdUsage(conf)
-					return nil, nil, fmt.Errorf("%v: %v", ErrCmd, cmd)
-				}
-				command.Name = cmd
-				commands = append(commands, &command)
-			}
-		}
-
-		// Command?
-		command, isCommand := conf.Commands.Get(cmd)
-		if isCommand {
-			command.Name = cmd
-			commands = append(commands, &command)
-		}
-
-		if !isTarget && !isCommand {
-			cmdUsage(conf)
-			return nil, nil, fmt.Errorf("%v: %v", ErrCmd, cmd)
-		}
-	}
-
-	return &network, commands, nil
-}
-
-func resolvePath(path string) string {
-	if path[:2] == "~/" {
-		usr, err := user.Current()
-		if err == nil {
-			path = filepath.Join(usr.HomeDir, path[2:])
-		}
-	}
-	return path
-}
-
 func main() {
 	flag.Parse()
 
@@ -238,6 +103,16 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func resolvePath(path string) string {
+	if path[:2] == "~/" {
+		usr, err := user.Current()
+		if err == nil {
+			path = filepath.Join(usr.HomeDir, path[2:])
+		}
+	}
+	return path
 }
 
 func runSupfile(options options, args []string, data []byte) error {
@@ -354,5 +229,129 @@ func runSupfile(options options, args []string, data []byte) error {
 
 	// Run all the commands in the given network.
 	return app.Run(network, vars, commands...)
+}
 
+// parseArgs parses args and returns network and commands to be run.
+// On error, it prints usage and exits.
+func parseArgs(args []string, conf *sup.Supfile) (*sup.Network, []*sup.Command, error) {
+	var commands []*sup.Command
+
+	if len(args) < 1 {
+		networkUsage(conf)
+		return nil, nil, ErrUsage
+	}
+
+	// Does the <network> exist?
+	network, ok := conf.Networks.Get(args[0])
+	if !ok {
+		networkUsage(conf)
+		return nil, nil, ErrUnknownNetwork
+	}
+
+	hosts, err := network.ParseInventory()
+	if err != nil {
+		return nil, nil, err
+	}
+	network.Hosts = append(network.Hosts, hosts...)
+
+	// Does the <network> have at least one host?
+	if len(network.Hosts) == 0 {
+		networkUsage(conf)
+		return nil, nil, ErrNetworkNoHosts
+	}
+
+	// Check for the second argument
+	if len(args) < 2 {
+		cmdUsage(conf)
+		return nil, nil, ErrUsage
+	}
+
+	// In case of the network.Env needs an initialization
+	if network.Env == nil {
+		network.Env = make(sup.EnvList, 0)
+	}
+
+	// Add default env variable with current network
+	network.Env.Set("SUP_NETWORK", args[0])
+
+	// Add default nonce
+	network.Env.Set("SUP_TIME", time.Now().UTC().Format(time.RFC3339))
+	if os.Getenv("SUP_TIME") != "" {
+		network.Env.Set("SUP_TIME", os.Getenv("SUP_TIME"))
+	}
+
+	// Add user
+	if os.Getenv("SUP_USER") != "" {
+		network.Env.Set("SUP_USER", os.Getenv("SUP_USER"))
+	} else {
+		network.Env.Set("SUP_USER", os.Getenv("USER"))
+	}
+
+	for _, cmd := range args[1:] {
+		// Target?
+		target, isTarget := conf.Targets.Get(cmd)
+		if isTarget {
+			// Loop over target's commands.
+			for _, cmd := range target {
+				command, isCommand := conf.Commands.Get(cmd)
+				if !isCommand {
+					cmdUsage(conf)
+					return nil, nil, fmt.Errorf("%v: %v", ErrCmd, cmd)
+				}
+				command.Name = cmd
+				commands = append(commands, &command)
+			}
+		}
+
+		// Command?
+		command, isCommand := conf.Commands.Get(cmd)
+		if isCommand {
+			command.Name = cmd
+			commands = append(commands, &command)
+		}
+
+		if !isTarget && !isCommand {
+			cmdUsage(conf)
+			return nil, nil, fmt.Errorf("%v: %v", ErrCmd, cmd)
+		}
+	}
+
+	return &network, commands, nil
+}
+
+func networkUsage(conf *sup.Supfile) {
+	w := &tabwriter.Writer{}
+	w.Init(os.Stderr, 4, 4, 2, ' ', 0)
+	defer w.Flush()
+
+	// Print available networks/hosts.
+	fmt.Fprintln(w, "Networks:\t")
+	for _, name := range conf.Networks.Names {
+		fmt.Fprintf(w, "- %v\n", name)
+		network, _ := conf.Networks.Get(name)
+		for _, host := range network.Hosts {
+			fmt.Fprintf(w, "\t- %v\n", host)
+		}
+	}
+	fmt.Fprintln(w)
+}
+
+func cmdUsage(conf *sup.Supfile) {
+	w := &tabwriter.Writer{}
+	w.Init(os.Stderr, 4, 4, 2, ' ', 0)
+	defer w.Flush()
+
+	// Print available targets/commands.
+	fmt.Fprintln(w, "Targets:\t")
+	for _, name := range conf.Targets.Names {
+		cmds, _ := conf.Targets.Get(name)
+		fmt.Fprintf(w, "- %v\t%v\n", name, strings.Join(cmds, " "))
+	}
+	fmt.Fprintln(w, "\t")
+	fmt.Fprintln(w, "Commands:\t")
+	for _, name := range conf.Commands.Names {
+		cmd, _ := conf.Commands.Get(name)
+		fmt.Fprintf(w, "- %v\t%v\n", name, cmd.Desc)
+	}
+	fmt.Fprintln(w)
 }
